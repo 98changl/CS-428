a219bf8a003ccd1c5a4263a6f408348309af41e7
Luis Figueroa <wclimb.lf@gmail.com>
2020-09-22 15:43:11 -0400

    Added dynamic Obstacles. agent stopping distance set to 1.5

---
+++ B1/Assets/Scripts/PatrolMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolMovement : MonoBehaviour
+{
+    float speed = 2.0f;
+    void Update()
+    {
+        transform.position += Time.deltaTime * speed * Vector3.right;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("agent"))
+        {
+            speed = speed * 1;
+        }
+        else
+        {
+            speed = speed * -1;
+        }
+    }
+}
