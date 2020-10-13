fbd2dd138dd939928ab1f75df33bb829b9743633
98changl <98changl@gmail.com>
2020-10-13 16:43:49 -0400

    Added AgentController, Direction, and CameraController
    AgentController and Direction currently works for moving animated skeleton.
    CameraController is used to make the Main Camera object behave as an isometric camera.

---
+++ B2/Assets/Scripts/AgentController.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    //private bool selected = false;
+    private Vector2 smoothPos = Vector2.zero;
+    private Vector2 velocity = Vector2.zero;
+
+    Animator animator;
+    NavMeshAgent agent;
+    Direction direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        direction = GetComponent<Direction>();
+        agent.updatePosition = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        Move();
+    }
+
+    void Move()
+    {
+        // set up position for ground plane
+        Vector3 worldPos = agent.nextPosition - transform.position;
+        Vector2 pos = new Vector2(Vector3.Dot(transform.right, worldPos), Vector3.Dot(transform.forward, worldPos));
+        
+        // filter the move
+        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        smoothPos = Vector2.Lerp(smoothPos, pos, smooth);
+
+        velocity = smoothPos / Time.deltaTime;
+
+        bool moving = false;
+        if (velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius)
+        {
+            moving = true;
+        }
+
+        // update animator
+        animator.SetBool("isMoving", moving);
+        animator.SetFloat("x", velocity.x);
+        animator.SetFloat("y", velocity.y);
+
+        if (direction)
+        {
+            direction.lookAtTarget = agent.steeringTarget + transform.forward;
+        }
+
+        if (worldPos.magnitude > agent.radius)
+        {
+            transform.position = agent.nextPosition - 0.9f * worldPos;
+        }
+    }
+
+    void OnAnimatorMove()
+    {
+        Vector3 pos = animator.rootPosition;
+        pos.y = agent.nextPosition.y;
+        transform.position = pos;
+    }
+
+    /*
+    // Detect if agent was clicked
+    void OnMouseDown()
+    {
+        //If the agent is not selected, set the color to red, else set it to green.
+
+        //If the agent is selected and was clicked again, deselect the agent.
+        if (selected == true)
+        {
+            selected = false;
+        }
+        //If the agent is not selected and is clicked, select the agent.
+        else
+        {
+            selected = true;
+        }
+    }
+    */
+}
---
+++ B2/Assets/Scripts/Direction.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Direction : MonoBehaviour
+{
+    public Transform head = null;
+    public Vector3 lookAtTarget;
+    public float blendTime = 0.2f;
+
+    private Vector3 lookAtPosition;
+    private float lookAtWeight = 0.0f;
+    private float speed = 1.0f;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        lookAtTarget = head.position + transform.forward;
+        lookAtPosition = lookAtTarget;
+    }
+
+    void OnAnimatorIK()
+    {
+        lookAtTarget.y = head.position.y;
+
+        float lookAtWeightTarget = 1f;
+        
+        Vector3 current = lookAtPosition - head.position;
+        Vector3 target = lookAtTarget - head.position;
+
+        // update rotate positions
+        current = Vector3.RotateTowards(current, target, speed * Time.deltaTime, float.PositiveInfinity);
+        lookAtPosition = head.position + current;
+        
+        // set look at for animator
+        lookAtWeight = Mathf.MoveTowards(lookAtWeight, lookAtWeightTarget, Time.deltaTime / blendTime);
+        animator.SetLookAtWeight(lookAtWeight, 0.8f, 0.75f, 0.2f, 0f);
+        animator.SetLookAtPosition(lookAtPosition);
+    }
+}
