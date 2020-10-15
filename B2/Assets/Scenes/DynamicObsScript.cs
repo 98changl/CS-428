03801ac2fddd19970e9c3f9075b8acd031f24eed
Luis Figueroa <wclimb.lf@gmail.com>
2020-10-15 16:50:56 -0400

    Added navmesh and offlinks mesh

---
+++ B2/Assets/Scenes/DynamicObsScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicObsScript : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * speed * Vector3.back;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If statement to make patrol bounce back and forth when collides with a wall.
+        if(other.gameObject.CompareTag("Agent"))
+        {
+            speed *= 1;
+        }
+        else
+        {
+            speed *= -1;
+        }
+    }
+}
