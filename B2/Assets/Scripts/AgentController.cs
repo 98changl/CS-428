87c0b6d389eec6be66420b43124260e9960de1ed
98changl <98changl@gmail.com>
2020-10-17 21:09:02 -0400

    Added off-mesh link jumps and multiple clicks feature to AgentController

--- B2/Assets/Scripts/AgentController.cs
+++ B2/Assets/Scripts/AgentController.cs
@@ -10,8 +10,14 @@ public class AgentController : MonoBehaviour
     public Renderer agentColor;
 
     private bool selected = false;
-    private Vector2 smoothPos = Vector2.zero;
-    private Vector2 velocity = Vector2.zero;
+    private bool blocked = false;
+    private float speed;
+    private float jumpAnimationBlend = 0;
+    private float previousClick = 0f;
+    private float timeBetweenClicks = 0.2f;
+
+    public Vector2 velocity = Vector2.zero;
+    public Vector2 prev_velocity = Vector2.zero;
 
     Animator animator;
     NavMeshAgent agent;
@@ -37,7 +43,21 @@ public class AgentController : MonoBehaviour
             {
                 agent.destination = hit.point;
             }
+
+            float click = Time.time - previousClick;
+            if (click <= timeBetweenClicks) // multiple clicks
+            {
+                Debug.Log("Run");
+                speed = 2f;
+            }
+            else
+            {
+                Debug.Log("Walk");
+                speed = 0.75f;
+            }
+            previousClick = Time.time;
         }
+
         Move();
     }
 
@@ -47,12 +67,24 @@ public class AgentController : MonoBehaviour
         Vector3 worldPos = agent.nextPosition - transform.position;
         Vector2 pos = new Vector2(Vector3.Dot(transform.right, worldPos), Vector3.Dot(transform.forward, worldPos));
         
-        // filter the move
-        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
-        smoothPos = Vector2.Lerp(smoothPos, pos, smooth);
+        if (pos.x > 0)
+        {
+            velocity.x = 1;
+        }
+        else
+        {
+            velocity.x = -1;
+        }
 
-        velocity = smoothPos / Time.deltaTime;
-
+        if (pos.y > 0)
+        {
+            velocity.y = 1;
+        }
+        else
+        {
+            velocity.y = -1;
+        }
+        
         bool moving = false;
         if (velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius)
         {
@@ -61,17 +93,55 @@ public class AgentController : MonoBehaviour
 
         // update animator
         animator.SetBool("isMoving", moving);
-        animator.SetFloat("x", velocity.x);
-        animator.SetFloat("y", velocity.y);
+        animator.SetFloat("x", velocity.x, .1f, Time.deltaTime);
+        animator.SetFloat("y", velocity.y * speed, .1f, Time.deltaTime);
 
+        // set object look direction
         if (direction)
         {
             direction.lookAtTarget = agent.steeringTarget + transform.forward;
         }
 
+        // move object to agent
         if (worldPos.magnitude > agent.radius)
         {
             transform.position = agent.nextPosition - 0.9f * worldPos;
+        }
+
+        Jump();
+        
+    }
+    /*
+    IEnumerator Blocked(Vector3 prevPos)
+    {
+        yield return new WaitForSeconds(0.3f);
+        
+        if (prevPos != transform.position)
+        {
+            blocked = true;
+            Debug.Log("Blocked");
+        }
+    }
+    */
+
+    void Jump()
+    {
+        if (agent.isOnOffMeshLink)
+        {
+            Debug.Log("Jump");
+            //velocity = Vector2.zero;
+            Vector3 Jump = new Vector3(0, 10, 0);
+            Jump.Normalize();
+            transform.localPosition += transform.TransformDirection(Jump * 30 * Time.deltaTime);
+
+            jumpAnimationBlend += 0.2f * Time.deltaTime;
+            animator.SetBool("isAirborne", true);
+            animator.SetFloat("Jump Blend", jumpAnimationBlend);
+        }
+        else
+        {
+            animator.SetBool("isAirborne", false);
+            jumpAnimationBlend = 0;
         }
     }
 
@@ -81,7 +151,7 @@ public class AgentController : MonoBehaviour
         pos.y = agent.nextPosition.y;
         transform.position = pos;
     }
-
+    
     //Detect if agent was clicked
     void OnMouseDown()
     {
@@ -101,5 +171,24 @@ public class AgentController : MonoBehaviour
         }
 
     }
-
+    /*
+    IEnumerator MultipleClicks()
+    {
+        float timer = 0f;
+        while (timer < timeBetween)
+        {
+            if (Input.GetMouseButtonDown(1) && selected == true)
+            {
+                Debug.Log("Run");
+                run = true;
+            }
+            else
+            {
+                Debug.Log("Walk");
+                run = false;
+            }
+        }
+        yield return null;
+    }
+    */
 }
