1c0855d3a98669b4a2153ea7d670c1535bcec305
Christopher Clemente <40449073+RogueWorm@users.noreply.github.com>
2020-11-19 17:25:47 -0500

    Updated scene and added camera/agent controller script

---
+++ B5/Assets/Scripts/DestinationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DestinationController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(agent.transform.position.x, agent.transform.position.y + 23, agent.transform.position.z - 14);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+
+                agent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
