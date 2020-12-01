40563b3fc8487e77ab8ea53c516f066592b6ddfd
Luis Figueroa <wclimb.lf@gmail.com>
2020-12-01 16:16:12 -0500

    added coins, added idle animation

---
+++ B5/Assets/CoinScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(90 * Time.deltaTime,0 ,0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Agent tag -> Player or Daniel
+        if (other.tag == "Daniel")
+        {
+            other.GetComponent<DisplayCoinsCollected>().coinsCollected++;
+            Destroy(gameObject);
+        }
+    }
+}
