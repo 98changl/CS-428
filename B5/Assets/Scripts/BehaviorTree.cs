f90d786a1deae799b414543ad448095d625f24ae
Christopher Clemente <40449073+RogueWorm@users.noreply.github.com>
2020-11-30 19:51:50 -0500

    Worked on the IBT and passing user interaction through the UI to the script

---
+++ B5/Assets/Scripts/BehaviorTree.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using TreeSharpPlus;
+
+public class BehaviorTree : MonoBehaviour
+{
+	public GameObject participant;
+	private BehaviorAgent behaviorAgent;
+
+    private int input = 0;
+
+    private enum StoryArc
+    {
+        ROOT = 0,
+        EVIL,
+        HACKER,
+        EXPLODE
+    }
+
+    private StoryArc currArc = StoryArc.ROOT;
+
+    // Use this for initialization
+    void Start ()
+	{
+		behaviorAgent = new BehaviorAgent (this.BuildTreeRoot ());
+		BehaviorManager.Instance.Register (behaviorAgent);
+		behaviorAgent.StartBehavior ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        //Debug.Log(input);
+        Debug.Log(currArc);
+	}
+
+    private Node ExplodeBombFirst()
+    {
+            return new Sequence(
+                new LeafAssert(() => input == 1),
+                new LeafInvoke(() => currArc = StoryArc.EXPLODE)
+                );
+        
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        Node roaming = new DecoratorLoop(
+            new Sequence(
+                new SelectorParallel(
+                    ExplodeBombFirst()
+                    )
+                )           
+        );
+		return roaming;
+	}
+
+    public void inputButton(int option)
+    {
+        input = option;
+    }
+
+}
--- B5/Assets/Scripts/DialogueManager.cs
+++ B5/Assets/Scripts/DialogueManager.cs
@@ -43,7 +43,7 @@ public class DialogueManager : MonoBehaviour
 
         button1.text = "Debugger";
 
-        Debug.Log(mainTextBox);
+        //Debug.Log(mainTextBox);
     }
 
 
@@ -53,15 +53,25 @@ public class DialogueManager : MonoBehaviour
         float distanceToEvil = (mainCharacter.transform.position - drEvil.transform.position).magnitude;
         float distanceToHacker = (mainCharacter.transform.position - hacker.transform.position).magnitude;
 
-        if(distanceToEvil <= 2.5f || distanceToHacker <= 2.5f)
+        if (distanceToEvil <= 2.5f || distanceToHacker <= 2.5f)
         {
             dialogueWindow.enabled = true;
         }
         else
         {
             dialogueWindow.enabled = false;
-        }
-        
+        }   
     }
+
+    void EvilPrinter()
+    {
+
+    }
+
+    void HackerPrinter()
+    {
+
+    }
+
 
 }
