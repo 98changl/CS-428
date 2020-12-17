8fa79b6e5905fa04d5386295c9f2d393afad9163
lfigil <50636010+lfigil@users.noreply.github.com>
2020-12-16 20:51:44 -0500

    Added single player folder
    Working on the maze

---
+++ P1/Assets/SinglePlayer/Scripts/CamCont.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCont : MonoBehaviour
+{
+    GameObject player;
+    Vector3 camOffset;
+    public int x;
+    public int y;
+    public int z;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        
+    }
+
+    private void Update()
+    {
+        camOffset = new Vector3(x, y, z);
+        transform.position = player.transform.position + camOffset;
+    }
+}
