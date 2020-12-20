937563b617931af402d2ab979128add524191200
Christopher Clemente <40449073+RogueWorm@users.noreply.github.com>
2020-12-20 00:40:57 -0500

    Tag animation added

--- P1/Assets/Multiplayer (Group2)/Scripts/PlayerController.cs
+++ P1/Assets/Multiplayer (Group2)/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviourPunCallbacks
     private float jumpAnimationBlend = 0;
     private float xAxis;
     private float gravity = 3f;
+    private float hitTimer = 0f;
     private Vector3 moveVector = Vector3.zero;
 
     Animator animator;
@@ -36,6 +37,7 @@ public class PlayerController : MonoBehaviourPunCallbacks
         {
             Mover();
             Look();
+            Tag();
         }
     }
 
@@ -43,6 +45,25 @@ public class PlayerController : MonoBehaviourPunCallbacks
     {
         xAxis += Input.GetAxis("Mouse X");
         transform.eulerAngles = new Vector2(0, xAxis);
+    }
+
+    public void Tag()
+    {
+        hitTimer += Time.deltaTime;
+
+        if (Input.GetMouseButton(0) && hitTimer > 2)
+        {
+            animator.SetBool("isClicked", true);
+            GetComponent<CapsuleCollider>().radius = 0.6f;
+            hitTimer = 0;
+        }
+        
+        if(hitTimer > 2)
+        {
+            animator.SetBool("isClicked", false);
+            GetComponent<CapsuleCollider>().radius = 0.2f;
+        }
+
     }
 
     private void Mover()
@@ -110,76 +131,4 @@ public class PlayerController : MonoBehaviourPunCallbacks
         }
 
     }
-
-
-    private void Move()
-    {
-        //Scan axis for inputs
-        float LeftRight = Input.GetAxisRaw("Horizontal");
-        float ForwardBackward = Input.GetAxisRaw("Vertical");
-
-        if (LeftRight == 0 && ForwardBackward == 0)
-        {
-            animator.SetBool("isMoving", false);
-        }
-        else
-        {
-            animator.SetBool("isMoving", true);
-        }
-
-        Debug.Log(ForwardBackward * speed * .12f);
-
-        animator.SetFloat("x", LeftRight, .1f, Time.deltaTime);
-        animator.SetFloat("y", ForwardBackward * speed * .12f, .1f, Time.deltaTime);
-
-        //Translate inputs to a vector and normalize movement speed
-        Vector3 Movement = new Vector3(LeftRight, 3, ForwardBackward);
-        Movement *= speed;
-        Movement.Normalize();
-
-        transform.localPosition += transform.TransformDirection(Movement * speed * Time.deltaTime);
-
-
-        //Check if character is grounded and add to gravity if it isnt
-        if (Controller.isGrounded == false)
-        {
-            moveVector += Physics.gravity;
-        }
-
-        Controller.Move(moveVector * Time.deltaTime * 0.01f);
-
-        //Get character to jump if he is on the ground and space is pressed.
-        if (Input.GetKeyDown(KeyCode.Space) && Controller.isGrounded)
-        {
-            moveVector = Vector3.zero;
-            Vector3 Jump = new Vector3(0, 5, 0);
-            Jump.Normalize();
-            transform.localPosition += transform.TransformDirection(Jump * 30 * Time.deltaTime);
-
-        }
-
-        if (Controller.isGrounded)
-        {
-            animator.SetBool("isAirborne", false);
-            jumpAnimationBlend = 0;
-        }
-        else
-        {
-            jumpAnimationBlend += 0.2f * Time.deltaTime;
-            animator.SetBool("isAirborne", true);
-            animator.SetFloat("Jump Blend", jumpAnimationBlend);
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 8;
-        }
-        else
-        {
-            speed = 4;
-        }
-
-    }
-
-
 }
