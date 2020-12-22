5aabfabae9cebcea03277e8cd5af584d330738e8
John Aceto <jpa102@rutgers.edu>
2020-12-22 10:40:58 -0500

    Finished scoring and colliding features

---
+++ P1/Assets/SinglePlayer/Scripts/PointsScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsScript : MonoBehaviour
+{
+    public GameObject[] players;
+    public Text playerScore;
+    public Text AIScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+            var initialPoints = 0;
+            playerScore.text = initialPoints.ToString();
+            AIScore.text = initialPoints.ToString();
+            players = GameObject.FindGameObjectsWithTag("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(players.Length);
+        foreach(GameObject player in players)
+        {
+            var TagScript = player.GetComponent<TagScript>();
+            var points = TagScript.points;
+            //Debug.Log(points.ToString());                        
+            //Debug.Log("Got Here");
+
+            if(player.name.Equals("Player"))
+            {
+                playerScore.text = points.ToString();
+            }
+            else
+            {
+                AIScore.text = points.ToString();
+            }
+        }
+    }
+}
--- P1/Assets/SinglePlayer/Scripts/TagScript.cs
+++ P1/Assets/SinglePlayer/Scripts/TagScript.cs
@@ -6,13 +6,17 @@ public class TagScript : MonoBehaviour
 {
     public bool isTagged;
 
-    private int points;
+    public int points;
     private float time;
     private int delay = 1;
+    private bool canBeTagged;
+    private float tagDelay = 2f;
     // Start is called before the first frame update
     void Start()
     {
         points = 0;
+        canBeTagged = false;
+        StartCoroutine(WaitForTag());
     }
 
     // Update is called once per frame
@@ -32,5 +36,59 @@ public class TagScript : MonoBehaviour
             }
             Debug.LogFormat("points = {0}", points);
         }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        if(other.gameObject.tag == "player" && canBeTagged)
+        {
+            Debug.Log("Player Trigger");
+            if(isTagged == false)
+            {
+                isTagged = true;
+            }
+            else
+            {
+                isTagged = false;
+            }
+            canBeTagged = false;
+            StartCoroutine(WaitForTag());
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+        if(collision.gameObject.tag == "player" && canBeTagged)
+        {
+            Debug.Log("Player Collision");
+            if(isTagged == false)
+            {
+                isTagged = true;
+            }
+            else
+            {
+                isTagged = false;
+            }
+            canBeTagged = false;
+            StartCoroutine(WaitForTag());
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+
+    }
+
+    private IEnumerator WaitForTag()
+    {
+        yield return new WaitForSeconds(tagDelay);
+        canBeTagged = true;
     }
 }
