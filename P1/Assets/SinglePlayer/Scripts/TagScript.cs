0b15fe3d6ad67d2993a47fad37a7204f42d67840
John Aceto <jpa102@rutgers.edu>
2020-12-21 19:54:33 -0500

    Added timer and points script to singleplayer

---
+++ P1/Assets/SinglePlayer/Scripts/TagScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagScript : MonoBehaviour
+{
+    public bool isTagged;
+
+    private int points;
+    private float time;
+    private int delay = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTagged)
+        {
+            // No points awarded
+        }
+        else
+        {
+            time += Time.deltaTime;
+            if(time >= delay)
+            {
+                time = 0f;
+                points++;
+            }
+            Debug.LogFormat("points = {0}", points);
+        }
+    }
+}
---
+++ P1/Assets/SinglePlayer/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+
+    public float timer = 120;
+    public bool isRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRunning = true;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isRunning)
+        {
+            if(timer > 0)
+            {
+                timer -= Time.deltaTime;
+                DisplayTime(timer);
+            }
+            else
+            {
+                Debug.Log("Time is out");
+                timer = 0;
+                isRunning = false;
+            }
+        }  
+    }
+
+    void DisplayTime(float time)
+    {
+        float min = Mathf.FloorToInt(time / 60);
+        float sec = Mathf.FloorToInt(time % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+}
